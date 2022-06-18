4af4ee783e08a073be9827210b06672f1d56543d
copperpocket <m.mulek45@gmail.com>
2022-06-18 10:24:10 -0500

    L5

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
             inventory: 0,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'},
+            ]
         }
     }
 })
