746d237d2d5db442f0fe3d23d12e2edc197fc1d8
copperpocket <m.mulek45@gmail.com>
2022-06-18 10:12:56 -0500

    L4

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+            url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+            inventory: 0,
+            onSale: true
         }
     }
 })
