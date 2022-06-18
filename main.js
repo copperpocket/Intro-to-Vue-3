de06d596e021fa35f47b42b3f4e46fd1c7f555f2
copperpocket <m.mulek45@gmail.com>
2022-06-18 09:40:24 -0500

    L3

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
+<<<<<<< Updated upstream
             product: 'Sand',
             description: "Course, get's everywhere"
+=======
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+>>>>>>> Stashed changes
         }
     }
 })
